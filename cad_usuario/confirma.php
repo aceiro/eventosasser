8600ceb90a98a425450682b2ffaeea353aab7248
Cristiano José Cecanho <cecanho@gmail.com>
2015-11-07 15:58:22 -0200

    Atualizado a confirmação de cadastro de usuario

--- cad_usuario/confirma.php
+++ cad_usuario/confirma.php
@@ -3,28 +3,58 @@
 <head>
 <meta charset="utf-8" />
 <title>Asser Eventos - Cadastro realizado com sucesso</title>
-<link REL=StyleSheet HREF="../css/estilo.css" TYPE="text/css"></head>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/redmond/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+<link rel="stylesheet" href="../css/menu-styles.css" type="text/css">
+<link rel="stylesheet" href="../css/estilo.css" type="text/css">
+
+<script src="../scripts/asser-main-menu.js"></script>
+<script src="../scripts/asser-commum.js"></script>
+<script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
 
 <body>
 	<div id="corpo">
     	
 		<div id="cabecalho">
-			IX - Semana Conhecimento e VI - Mostra de Iniciação Científica
-		</div>
+            IX - Semana Conhecimento
+            <div id="subcabecalho" style="font-size:14px"> VI Mostra de Iniciação Científica </div>
+        </div>
         
         <br />
         
-        <div id="mmenu">
-		<a>Dados Pessoais</a> | <a>Dados Resumo</a>  | Resumo Enviado</div>
-        
+          <div id='cssmenu'>
+                <ul>
+                   <li class='active'><a href='../index.html'>Submissão de Resumos</a></li>
+                   <li><a href='../palestra'>Palestras</a></li>
+                   <li><a href='../programa.html'>Programação</a></li>            
+                   <li><a href='#'>Sobre o evento</a></li>             
+                   <li><a href='../adm'>Administrativo</a></li>
+                   <li><a href='../contato'>Contato</a></li>
+                   <li><a href='../creditos.html'>Créditos</a></li>
+                </ul>
+            </div>
+
+            <div id="mmenu"> &nbsp;</div>
+            <div id="mmenubar"> &nbsp;</div>
+            <div id="mmenusubbar"> &nbsp;</div>
+            <div id="mmenusubsubbar"> &nbsp;</div>
+
         <br />
         
         <div id="texto">
 		    <form id="cad_resumo" name="resumo" method="post" action="confirma.php"  onSubmit="return validaCampo(); return false;">
-            	<p align="center">Cadastro realizado com sucesso!</p>
+            	<fieldset>
+				<legend>Cadastro realizado com sucesso!</legend>
+				<br />
 				
+				<p id="effect" class="ui-corner-all">Agora você pode verificar o andamento da avaliação do seu resumo. Basta clicar em Verificar resumo.</p>
+				
+				<br />
 				<p align="center"><a href="../">Voltar</a> | <a href="../listar">Verificar resumo</a></p>
-
+			</fieldset>
             </form>
 		</div>
         
