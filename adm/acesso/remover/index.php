8152b4122d4795dafe98e636b99a308a5d91a294
Cristiano José Cecanho <cecanho@gmail.com>
2015-11-17 18:50:13 -0200

    Adição de uma página de remoção

---
+++ adm/acesso/remover/index.php
@@ -0,0 +1,92 @@
+<?php $config = require '../../../cfg/config.php'; ?>
+<!DOCTYPE html >
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8" />
+<title>Asser Eventos - Remoção de Resumo</title>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/redmond/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+<link rel="stylesheet" href="../../../css/menu-styles.css" type="text/css">
+<link rel="stylesheet" href="../../../css/estilo.css" type="text/css">
+
+<script src="../../../scripts/asser-main-menu.js"></script>
+<script src="../../../scripts/asser-commum.js"></script>
+<script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
+
+<body>
+	<div id="corpo">
+    	
+        <div id="cabecalho">
+        </div>
+        
+        <br />
+
+        <!-- inicio do menu da aplicacao -->
+          <div id='cssmenu'>
+                <ul>
+                   <li><a href='#'>Evento</a></li>   
+                   <li class='active'><a href='../'>Administração</a></li>
+                   <li><a href='../palestra'>Palestras</a></li>
+                   <li><a href='../programa.html'>Programação</a></li>            
+                        
+                   <li class='has-sub'> <a href='#'>Edições Anteriores</a> 
+                      <ul>
+                        <li> <a href='../anais/Anais2015_FINAL.pdf' target="_blank"> V Mostra de Iniciação Científica e Workshop (Anais 6/2015)</a> </li>
+                      </ul>
+                   </li>             
+                   <li><a href='../contato'>Contato</a></li>
+                   <li><a href='../creditos.html'>Créditos</a></li>
+                </ul>
+            </div>
+
+            <!-- adiciona o suporte ao separador gradiente -->
+            <div id="mmenu"> &nbsp;</div>
+            <div id="mmenubar"> &nbsp;</div>
+            <div id="mmenusubbar"> &nbsp;</div>
+            <div id="mmenusubsubbar"> &nbsp;</div>
+            <br /> 
+        
+        <div id="texto">
+    		<form id="register-form" name="register-form" method="post" action="av_resumo.php"  >
+                <fieldset>
+				<legend>Remover resumo duplicado</legend>
+				<p align="center"><b>Digite o ID do resumo a remover.</b></p>
+				<p align="center"><input type="text" name="id" size="19" maxlength="20" /><input name="avaliar" type="submit" id="avaliar" value="Avaliar" /></p>
+				<?php
+				// Estabelecendo a conexão com o banco de dados
+				try{
+                    $link = new PDO($config['dsn'], $config['dbuser'], $config['dbpass']);
+					$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+					$sql = 'SELECT * FROM evento order by curso';
+					
+					echo '<table style="width:100%">';
+					echo '<tr><td>ID</td><td>TITULO</td><td>ALUNO</td><td>CURSO</td><td>STATUS</td></tr>';
+					
+					foreach($link->query($sql) as $row){
+						echo "<tr><td>".$row['id']."</td>";
+						echo '</td><td>'.$row['titulo'].'</td><td>'.$row['nome'].'</td><td>'.$row['curso'].'</td><td>'.$row['status'].'</td></tr>';	
+					}
+					echo '</table>';
+					
+				}catch(PDOException $e){
+					echo "ERROR" . $e->getMessage();
+				}
+				?>				
+                
+				<br />	
+			</fieldset>
+            </form>
+        </div>
+        
+        <br />
+        
+        <div id="rodape">
+    	<p>Campus Rio Claro: Rua 7, 1193 - Centro - CEP 13500-200 - Fone/ Fax: (19) 3523-2001 © 2006-2013, ASSER - Todos os direitos reservados  Visualização: 800 x 600 - Desenvolvido pelo Curso de Sistemas de Informação. </p>
+    	</div>
+    </div>
+</body>
+</html>
