c260e15e4f2e7ae283926b07130dd0c9dce92223
Erik Aceiro Antonio <aceiro@gmail.com>
2015-12-11 11:47:23 -0200

    correcao na listagem com adicao de css para o botao

---
+++ relatorios/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html >
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="cache-control" content="no-store" />
+    <link rel="shortcut icon" href="../../favicon.ico">
+    <title>Asser Eventos</title>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/redmond/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+<link rel="stylesheet" href="../css/menu-styles.css" type="text/css">
+<link rel="stylesheet" href="../css/estilo.css" type="text/css">
+
+<script src="../scripts/asser-main-menu.js"></script>
+<script src="../scripts/asser-commum.js"></script>
+<script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
+
+<body>
+	<div id="corpo">
+    	
+        <div id="cabecalho">
+        </div>
+        
+        <br />
+
+        <!-- inicio do menu da aplicacao -->
+        <div id='cssmenu'>
+            <ul>
+                <li><a href='../index.html'>Evento</a></li>
+                <li><a href='../submissao.html'>Submissão de Trabalhos</a></li>
+                <li><a href='../palestra'>Palestras</a></li>
+                <li class='active'><a href='#'>Programação</a></li>
+                <li class='has-sub'> <a href='#'>Edições Anteriores</a>
+                    <ul>
+                        <li> <a href='../anais/Anais2015_FINAL.pdf' target="_blank"> V Mostra de Iniciação Científica e Workshop (Anais 6/2015)</a> </li>
+                    </ul>
+                </li>
+
+                <li><a href='../contato'>Contato</a></li>
+                <li><a href='../creditos.html'>Créditos</a></li>
+            </ul>
+        </div>
+
+        <!-- adiciona o suporte ao separador gradiente -->
+        <div id="mmenu"> &nbsp;</div>
+        <div id="mmenubar"> &nbsp;</div>
+        <div id="mmenusubbar"> &nbsp;</div>
+        <div id="mmenusubsubbar"> &nbsp;</div>
+        <br />
+        <!-- fim do menu da aplicação -->
+
+        <style type="text/css">
+
+            li {
+                display: inline-block;
+                vertical-align: middle;
+            }
+
+            .button {
+
+                display: block;
+                width: 115px;
+                height: 25px;
+                background: #336799;
+                padding: 10px;
+                text-align: center;
+                border-radius: 5px;
+                color: white;
+                font-weight: bold;
+
+                text-decoration: none;
+
+            }
+            div a:hover{
+                font-size: 80%;
+                border:medium;
+            }
+
+
+
+        </style>
+        <div align="center">
+        <ul>
+            <li> <a class="button" href="pedagogia.php"> Pedagogia </a> &nbsp;&nbsp; </li>
+            <li> <a class="button" href="edfisica.php"> Educação Física  </a>  &nbsp;&nbsp; </li>
+            <li> <a class="button" href="engprod.php"> Engenharia de Produção  </a>  &nbsp;&nbsp; </li>
+            <li> <a class="button" href="nutri.php"> Nutrição </a> &nbsp;&nbsp; </li>
+            <li> <a class="button" href="arquitetura.php"> Arquitetura e Urbanismo </a> &nbsp;&nbsp; </li>
+        </ul>
+        </div>
+
+
+        <br />
+        <div id="rodape">
+    	<p>Campus Rio Claro: Rua 7, 1193 - Centro - CEP 13500-200 - Fone/ Fax: (19) 3523-2001 © 2006-2013, ASSER - Todos os direitos reservados  Visualização: 800 x 600 - Desenvolvido pelo Curso de Sistemas de Informação. </p>
+    	</div>
+    </div>
+</body>
+</html>
