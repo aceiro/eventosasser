78420be4bbaadf1b981d9a75aeab2376871ac5f3
Cristiano José Cecanho <cecanho@gmail.com>
2016-10-14 18:17:04 -0300

    O charset era utf-8, no meu repositório deu erro ortográfico

--- cad_usuario/perfil.php
+++ cad_usuario/perfil.php
@@ -1,7 +1,7 @@
 <?php
     require_once("../cfg/Session.php");
     $session = new Session("EventosAsser2016");
-    header('Content-Type: text/html; charset=iso-8859-1');
+header('Content-Type: text/html; charset=UTF-8');
 ?>
 
 <!DOCTYPE html >
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/redmond/jquery-ui.css">
     <script src="//code.jquery.com/jquery-1.10.2.js"></script>
     <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-    <!-- outros suporte a css da página -->
+    <!-- outros suporte a css da pï¿½gina -->
     <link rel="stylesheet" href="../css/menu-styles.css" type="text/css">
     <link rel="stylesheet" href="../css/estilo.css" type="text/css">
     <!-- outros scripts para o menu-->
@@ -39,8 +39,8 @@
                 },
                 messages: {
                     nome: "Escreva o seu nome completo",
-                    email: "Escreva seu endereço de email corretamente",
-                    tipo: "Escolha um tipo de participação",
+                    email: "Escreva seu endereï¿½o de email corretamente",
+                    tipo: "Escolha um tipo de participaï¿½ï¿½o",
                     senha: {
                         required: "Por favor, digite uma senha",
                         minlength: "Sua senha deve ter mais de 5 caracteres"
@@ -75,11 +75,11 @@
     <div id='cssmenu'>
         <ul>
             <li><a href='../index.html'>Evento</a></li>
-            <li class='active'><a href='../index.html'>Inscrição no evento</a></li>
-            <li><a href='../programa.html'>Programação</a></li>
-            <li> <a href='../anais'>Edições<br>Anteriores</a></li>
+            <li class='active'><a href='../index.html'>InscriÃ§Ã£o no evento</a></li>
+            <li><a href='../programa.html'>ProgramaÃ§Ã£o</a></li>
+            <li> <a href='../anais'>EdiÃ§Ãµes<br>Anteriores</a></li>
             <li><a href='../contato'>Contato</a></li>
-            <li><a href='../creditos.html'>Créditos</a></li>
+            <li><a href='../creditos.html'>CrÃ©ditos</a></li>
         </ul>
     </div>
 
@@ -87,7 +87,7 @@
     <div id="mmenubar"> &nbsp;</div>
     <div id="mmenusubbar"> &nbsp;</div>
     <div id="mmenusubsubbar"> &nbsp;</div>
-    <div style="text-align: right"><?php echo "Olá " . $session->get('nome');?></div>
+    <div style="text-align: right"><?php echo "OlÃ¡ " . $session->get('nome');?></div>
     <div style="padding: 50px; margin-bottom: 50px; height: 100px;">
         <div style="height: 150px; width: 33%; float: left;">
             <fieldset style="background-color: #e6EEEE; width: 80%">
@@ -135,7 +135,7 @@
     </div>
 
     <div id="rodape">
-        <p>Campus Rio Claro: Rua 7, 1193 - Centro - CEP 13500-200 - Fone/ Fax: (19) 3523-2001 © 2006-2013, ASSER - Todos os direitos reservados  <br/> Desenvolvido pelo <a href="http://www.asser.edu.br/rioclaro/graduacao/sistemas/" target="_new"> Curso de Sistemas de Informação </a> </p>
+        <p>Campus Rio Claro: Rua 7, 1193 - Centro - CEP 13500-200 - Fone/ Fax: (19) 3523-2001 ï¿½ 2006-2013, ASSER - Todos os direitos reservados  <br/> Desenvolvido pelo <a href="http://www.asser.edu.br/rioclaro/graduacao/sistemas/" target="_new"> Curso de Sistemas de Informaï¿½ï¿½o </a> </p>
     </div>
 </div>
 </body>
