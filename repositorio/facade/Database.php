8e319b5fd32ba8eaf0eac61507be19b620594fae
aceiro <aceiro@gmail.com>
2018-10-15 21:33:38 -0300

    Fix for release v3.0.0

--- repositorio/facade/Database.php
+++ repositorio/facade/Database.php
@@ -2,7 +2,7 @@
 
 require '../RedBeanPHP4_3_2/rb.php';
 // DEV Environment at Local laptop
-R::setup( 'mysql:host=localhost;dbname=eventosa_v1', 'eventosa_root', '6I97k$ERv@TD' );
+R::setup( 'mysql:host=localhost;dbname=eventosa_v1', '', '' );
 
 
 
