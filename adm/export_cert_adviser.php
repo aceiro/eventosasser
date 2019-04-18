2f2c902c897762df503db4120b15555b8d855a1a
Erik Aceiro Antonio @e2a <aceiro@gmail.com>
2019-04-18 14:38:33 -0300

    EV2019#60 - Geração automatica de certificados para orientadores

--- adm/export_cert_adviser.php
+++ adm/export_cert_adviser.php
@@ -10,6 +10,7 @@ $summaryRepository     = EventosAsserFacade::createTrabalhoRepository();
 $adviserRepository     = EventosAsserFacade::createOrientadorRepository();
 $participantSummaryRepository = EventosAsserFacade::createParticipanteTrabalhoRepository();
 $courseRepository = EventosAsserFacade::createCursoRepository();
+$studentSubscriptionRepository  = EventosAsserFacade::createInscricaoRepository();
 
 
 
@@ -20,138 +21,131 @@ header("Content-Type: text/html; charset=UTF-8", true);
 <!DOCTYPE html >
 <html lang="pt-BR">
 <head>
-    
+    <style type="text/css" media="print">
+        @page { 
+            size: landscape;
+        }
+        body { 
+            writing-mode: tb-rl;
+        }
+
+        
+</style>
 </head>
 
 <body>
 <div id="">
+    <?php 
+        $year = '2018'; 
+        $header = $studentSubscriptionRepository->findCertificateHeaderByYear($year);
+        $text   = "Certificamos para os devidos fins que <b>[onshow.fullname]</b> participou da “X MOSTRA DE INICIAÇÃO CIENTÍFICA E SEMANA DO CONHECIMENTO” que ocorreu nos dias 12, 13 e 14 de novembro de 2018 nas dependências da  Faculdade ASSER de Rio Claro. Como ORIENTADOR de trabalho(s) científicos (vide verso)";
 
+        $html = "
+             <img src=\"../imagens/$header\" style=\"height: 30%; width: 85%;\"/>
+                <br/>
+                <div id=\"header\">
+                    <p>
+                    <h1 style=\"text-align: center\"> Certificado de Orientação</h1>
+                    </p>
+                    <br/>
+                    <hr/>
+                </div>
+                <div id=\"content\">
+                    <p>
+                    <h3 style=\"text-align: justify;
+                               font-style: normal;
+                               font-weight: 100;
+                               font-size: large;
+                               padding: 5px 5px 5px 5px;
+                               line-height: 30px\">
+
+                    $text
+                     
+                    </h3>
+                    </p>
+                    <br/>
+                    <p>
+                    <h3 style=\"text-align: center\"> Rio Claro, [onshow.datefmt] </h3>
+                    </p>
+                    <p>
+                    <br/>
+                    <hr style=\"width: 40%\">
+                    <h3 style=\"text-align: center\"> Prof. Dr. Erik Aceiro Antonio (coordenador)</h3>
+                    </p>            
+                </div>
+
+                <div id=\"footer\">
+                </div> 
+        ";
+    ?>
     
     <?php
             $isToShowCourse = array();
-            $results = $summaryRepository->findAllSummariesIds();
-            foreach ($results as $result) {
-                $summaryId = $result['id']; 
-                $summary = $summaryRepository->findOne( $summaryId ) ;
-
-                $summaryId        = $summary->id;
-                $summaryTitle     = $summary->titulo;
-                $summaryBody      = $summary->resumo;
-                $summaryKeywords  = $summary->palavras_chave;
-                $adviserId        = $summary->id_orientador;
-                $coAdviserId      = $summary->id_coorientador;
-                $courseId         = $summary->id_curso;
+            $results = $summaryRepository->findAllAdviserPerYear();
+            foreach ($results as $result) {              
+                $adviserName  = $result['orientador'];
+                $adviserId    = $result['id'];   
                
+                $summaries = $summaryRepository->findAllSummariesById( $adviserId );
+                $summariesDetails =  array();
+                foreach ($summaries as $summary) {
+                    $summaryId    = $summary['id'];
+                    $summaryTitle = $summary['titulo'];
 
 
-                $adviser      = $adviserRepository->findOne($adviserId);
-                $coAdviser    = $adviserRepository->findOne($coAdviserId);
-                $adviserName  = $adviser->nome;
-                $course       = $courseRepository->findOne($courseId);
-                
+                    //
+                    // build authors and emails list
+                    $autors = $participantSummaryRepository->findAutoresByTrabalhoId( $summaryId );
+                    $autorsDetails = "";
 
-                //
-                // check if exists adviser
-                //
-                if( isset($adviser) ) {
-                    $adviserName = $adviser->nome;
-                } else {
-                    $adviserName = "";
-                }
-
-                //
-                // check if exists co-adviser
-                //
-                if( isset($coAdviser) ) {
-                    $coAdviserName = $coAdviser->nome;    
-                }else{
-                    $coAdviserName = "";    
-                }
-
-
-
-                //
-                // check if exists adviser and co-adviser are not same
-                // and if they are not empty
-                //
-                $isToShowCoAdviser = false;
-                if ( strlen($coAdviserName)>0 && $adviserName != $coAdviserName) {
-                    $isToShowCoAdviser = true;
-                }
-
-
-
-                //
-                // build authors and emails list
-                $autors        = $participantSummaryRepository->findAutoresByTrabalhoId($summaryId);
-                $autorsDetails = "";
-
-                $autorSize      = count($autors);
-                $pos            = 1;
-                foreach ($autors as $autor) {
-                    $autorsDetails.= ' '. $autor['nome']. ' - ' . $autor['email'] ;
-                    $pos++;
-                    if($pos <= $autorSize)
-                        $autorsDetails.=';';
-                }
-
-
-                //
-                // print course header on the left side
-                //
-                if( !isset( $isToShowCourse["$courseId"]) ){
-                    echo '<section>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<br/>';
-                    echo '<p style="text-align: right; font-size: x-large; font-weight: bold">'. $course['nome'] . ' </p>';
-                    $isToShowCourse["$courseId"] = 'true';
-                    echo '<br style="page-break-before: always">';
-                    echo '</section>';
+                    $autorSize      = count($autors);
+                    $pos            = 1;
+                    foreach ($autors as $autor) {
+                      
+                        $autorsDetails.= ' '. $autor['nome'];
+                        $pos++;
+                        if($pos <= $autorSize)
+                            $autorsDetails.=';';
+                    }
+                    
+                    array_push($summariesDetails, '<div><br>'.$summaryTitle.'</br>' . '<i>'.$autorsDetails.'</i></div>');
 
                 }
 
-                echo '<p style="text-align: center; font-size: large; font-weight: bold">'. $summaryTitle . '</p>';
-                echo '<p style="text-align: center; font-size: small">'. $autorsDetails . '</p>';
-                echo '<p style="text-align: center; font-size: small"> (O) '. $adviserName . '</p>';
-                
-                if( $isToShowCoAdviser ){
-                   echo '<p style="text-align: center; font-size: small"> (C) '. $coAdviserName . '</p>';
-                }
 
-                echo '<br/>';
-                echo '<p style="text-align: center; font-size: large; margin-top: 5px; bold"> RESUMO </p>';
-                echo '<p style="text-align: justify; margin-left: 50px; margin-right: 50px; font-size: small ">' . $summaryBody . '</p>';
-                echo '<p style="text-align: left; font-size: large; font-weight: bold; margin-left: 50px;">
-                    Palavras-chave: ' . $summaryKeywords . '</p>';
+                $html_aux = $html;
+                $html_aux = str_replace("[onshow.fullname]", $adviserName, $html_aux);
+                $html_aux = str_replace("[onshow.datefmt]", "18 de abril de 2019", $html_aux);
 
-                echo '<br style="page-break-before: always">';
+             
+               echo $html_aux;
+
+               // print summaries list 
+               // print each of summary details
+               echo '<br style="page-break-before: always"/>';
+               echo '<br style="page-break-before: always"/>';
+
+               $link = htmlentities("http:\\<eventosasser.com.br/anais/anais_2sem_2018.pdf>");
+               echo "
+               <strong>Trabalhos</strong>
+               <hr/>
+               <div style='text-align: justify'> 
+                            Como citar em ABNT <br/>
+
+                            SOBRENOME, PRENOME abreviado. Título: subtítulo (se houver). In: X - MOSTRA DE INICIAÇÃO CIENTÍFICA, 10., 2018, Rio Claro. <b>Anais...</b> São Paulo: ASSER Rio Claro, 2018. Disponível em $link
+                </div>";
+
+               foreach ($summariesDetails as $summary) {
+                 echo $summary;
+                 echo "<br/>";
+                 
+               }
+               echo "<hr/>";
+               echo '<br style="page-break-before: always"/>';
+               //
+               // end
             }
+
             ?>
 </div>
 </body>
